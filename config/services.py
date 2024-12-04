4edc75b79cfed67251d59855c52c6b10cfd428dc
AhnJoonSung <tjdtna01@naver.com>
2024-12-05 00:31:15 +0900

    :bug: fix: user service 호출 시 401 반환되던 문제 수정
    User service에서 Authorization 헤더를 안 받는데 포함해서 전송해서 나던
    에러

--- config/services.py
+++ config/services.py
@@ -2,12 +2,12 @@ import aiohttp
 
 USER_SERVICE = "http://localhost:8000/api/user/"
 
-async def get_user(user_id, token):
+async def get_user(user_id):
     user_service_url = f'{USER_SERVICE}{user_id}/'
-    headers = {'Authorization': f'Bearer {token}'}
+    # headers = {'Authorization': f'Bearer {token}'}
     
     async with aiohttp.ClientSession() as session:
-        async with session.get(user_service_url, headers=headers, timeout=10) as response:
+        async with session.get(user_service_url, timeout=10) as response:
             if response.status == 200:
                 return await response.json()
             return None
