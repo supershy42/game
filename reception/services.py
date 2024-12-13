7bb983f545ad76f93c79989cdc464010be6a55fb
AhnJoonSung <tjdtna01@naver.com>
2024-12-13 23:16:38 +0900

    :sparkles: feat: Reception Join에서 검증 추가
    - 인원 검증
    - 초대 여부 검증
    - 비밀 번호 검증

--- reception/services.py
+++ reception/services.py
@@ -1,10 +1,21 @@
-from django.urls import reverse
-
-def get_participants_key(reception_id):
-    return f'reception_{reception_id}_participants'
-
-def get_user_reception_key(user_name):
-    return f'user_{user_name}_reception'
+from config.custom_validation_error import CustomValidationError
+from config.error_type import ErrorType
+from .models import Reception
+from .redis_utils import get_participants_count, is_invited
 
 def websocket_reception_url(reception_id):
-    return reverse("websocket_reception", kwargs={"reception_id": reception_id})
+    return f"/ws/reception/{reception_id}/"
+
+async def validate_join_reception(reception_id, user_id, password):
+    try:
+        reception = await Reception.objects.aget(id=reception_id)
+    except:
+        raise CustomValidationError(ErrorType.RECEPTION_NOT_FOUND)
+    if await get_participants_count(reception_id) >= reception.max_players:
+        raise CustomValidationError(ErrorType.RECEPTION_FULL)
+
+    invited = await is_invited(reception_id, user_id)
+    print(invited)
+    
+    if not invited and not reception.check_password(password):
+        raise CustomValidationError(ErrorType.INVALID_PASSWORD)
--- reception/views.py
+++ reception/views.py
@@ -1,11 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import ReceptionSerializer
+from .serializers import ReceptionSerializer, ReceptionJoinSerializer
 from rest_framework.generics import ListAPIView
 from .models import Reception
-from .services import websocket_reception_url
+from .services import websocket_reception_url, validate_join_reception
 from .jwt_utils import create_ws_token
+from config.response_builder import response_error, response_ok
+from config.custom_validation_error import CustomValidationError
+from asgiref.sync import async_to_sync
 
 class CreateReceptionView(APIView):
     def post(self, request):
@@ -28,15 +31,25 @@ class ReceptionListView(ListAPIView):
     
 class ReceptionJoinView(APIView):
     def post(self, request, reception_id):
+        serializer = ReceptionJoinSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         user_id = request.user_id
-        user_password = request.data.get('password', None)
-        
-        reception = Reception.objects.get(id=reception_id)
-        
-        invited = False # temp
-        
-        if invited or reception.check_password(user_password):
-            ws_token = create_ws_token(user_id, reception_id)
-            ws_url = websocket_reception_url(reception_id)
-            return Response({"status": "ok", "ws_url": ws_url, "ws_token": ws_token}, status=status.HTTP_200_OK)
-        return Response({"detail": "Invalid password"}, status=status.HTTP_403_FORBIDDEN) 
+        password = serializer.validated_data.get('password')
+        try:
+            async_to_sync(validate_join_reception)(reception_id, user_id, password)
+            message = {
+                "reception_ws_url": websocket_reception_url(reception_id),
+                "invite_token": create_ws_token(user_id, reception_id)
+            }
+            return response_ok(message=message)
+        except CustomValidationError as e:
+            return response_error(e)
+
+# 테스트 용 임시 invitation view
+from .redis_utils import redis_invite
+class ReceptionInvitationView(APIView):
+    def post(self, request):
+        to_user_id = request.data.get('user_id')
+        reception_id = 2
+        async_to_sync(redis_invite)(reception_id, to_user_id)
+        return response_ok("test invite view.")
