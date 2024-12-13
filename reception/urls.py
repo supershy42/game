80f91b8a8f551049f1d9a5aabf494316bd209ee1
AhnJoonSung <tjdtna01@naver.com>
2024-12-13 18:20:41 +0900

    :hammer: refactor: reception url 변경

--- reception/urls.py
+++ reception/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CreateReceptionView, ReceptionListView, ReceptionJoinView
 
 urlpatterns = [
-    path('/create/', CreateReceptionView.as_view(), name='create-reception'),
-    path('s/', ReceptionListView.as_view(), name='receptions'),
-    path('/<int:reception_id>/join/', ReceptionJoinView.as_view(), name='join-reception'),
+    path('create/', CreateReceptionView.as_view(), name='create-reception'),
+    path('list/', ReceptionListView.as_view(), name='receptions'),
+    path('<int:reception_id>/join/', ReceptionJoinView.as_view(), name='join-reception'),
 ]
