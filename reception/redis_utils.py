eb2accee401af03b77d8fbfb698f995bdac59636
AhnJoonSung <tjdtna01@naver.com>
2024-12-14 15:50:15 +0900

    :hammer: refactor: redis_data field 값 변경
    기존: user_name
    변경: user_id
    
    다른 곳에서 user_name을 알려면 추가 비용이 발생해서 user_id로 가져올 수
    있게 변경.
    이에 따라 기존 get_participants가 user_id를 반환하게 돼서
    user_name을 반환하는 get_participants_info 함수 추가

--- reception/redis_utils.py
+++ reception/redis_utils.py
@@ -5,36 +5,36 @@ redis_client = redis.Redis(host='localhost', port=6379, db=0)
 def get_participants_key(reception_id):
     return f'reception_{reception_id}_participants'
 
-def get_user_reception_key(user_name):
-    return f'user_{user_name}_reception'
+def get_user_reception_key(user_id):
+    return f'user_{user_id}_reception'
 
 def get_invitation_key(reception_id, user_id):
     return f'invitation:{reception_id}:{user_id}'
 
-async def is_user_in_reception(user_name):
-    user_reception_key = get_user_reception_key(user_name)
+async def is_user_in_reception(user_id):
+    user_reception_key = get_user_reception_key(user_id)
     existing_reception = await redis_client.get(user_reception_key)
     if existing_reception:
         return True
     return False
 
-async def add_user_to_reception(reception_id, user_name):
+async def add_user_to_reception(reception_id, user_id):
     participants_key = get_participants_key(reception_id)
-    await redis_client.hset(participants_key, user_name, 0)
+    await redis_client.hset(participants_key, user_id, 0)
     
-    user_reception_key = get_user_reception_key(user_name)
+    user_reception_key = get_user_reception_key(user_id)
     await redis_client.set(user_reception_key, reception_id)
     
-async def remove_user_from_reception(reception_id, user_name):    
+async def remove_user_from_reception(reception_id, user_id):    
     participants_key = get_participants_key(reception_id)
-    await redis_client.hdel(participants_key, user_name)
+    await redis_client.hdel(participants_key, user_id)
     
-    user_reception_key = get_user_reception_key(user_name)
+    user_reception_key = get_user_reception_key(user_id)
     await redis_client.delete(user_reception_key)
     
-async def update_user_state(reception_id, user_name, is_ready):
+async def update_user_state(reception_id, user_id, is_ready):
     participants_key = get_participants_key(reception_id)
-    await redis_client.hset(participants_key, user_name, int(is_ready))
+    await redis_client.hset(participants_key, user_id, int(is_ready))
     
 async def should_remove_reception(reception_id):
     participants = await get_participants(reception_id)
@@ -54,6 +54,9 @@ async def get_participants(reception_id):
     participants_key = get_participants_key(reception_id)
     participants = await redis_client.hgetall(participants_key)
     return {k.decode(): int(v.decode()) for k, v in participants.items()}
+
+# async def get_current_reception(user_id):
+    
 
 async def get_participants_count(reception_id):
     return len(await get_participants(reception_id))
--- reception/services.py
+++ reception/services.py
@@ -1,10 +1,24 @@
+import asyncio
 from config.custom_validation_error import CustomValidationError
 from config.error_type import ErrorType
 from .models import Reception
-from .redis_utils import get_participants_count, is_invited
+from .redis_utils import get_participants, get_participants_count, is_invited, redis_invite
+from config.services import get_user
 
 def websocket_reception_url(reception_id):
     return f"/ws/reception/{reception_id}/"
+
+async def get_participants_detail(reception_id, token):
+    participants = await get_participants(reception_id)
+    tasks = {k: get_user(k, token) for k in participants.keys()}
+    users = await asyncio.gather(*tasks.values())
+    return {user.get('nickname'): participants[k] for k, user in zip(tasks.keys(), users)}
+    # result = {}
+    # for k, v in participants.items():
+    #     user = await get_user(k)
+    #     nickname = user.get('nickname')
+    #     result[nickname] = v
+    # return result
 
 async def validate_join_reception(reception_id, user_id, password):
     try:
@@ -15,7 +29,9 @@ async def validate_join_reception(reception_id, user_id, password):
         raise CustomValidationError(ErrorType.RECEPTION_FULL)
 
     invited = await is_invited(reception_id, user_id)
-    print(invited)
     
     if not invited and not reception.check_password(password):
         raise CustomValidationError(ErrorType.INVALID_PASSWORD)
+    
+# async def validate_invitation(from_user_id, to_user_id):
+#     reception_id = 
