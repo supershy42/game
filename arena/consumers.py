fede99b90f117e0efa5940a4c68ad4ac703ce525
AhnJoonSung <tjdtna01@naver.com>
2024-12-17 18:01:50 +0900

    :sparkles: feat: arena consumer prototype 작성

--- arena/consumers.py
+++ arena/consumers.py
@@ -1,39 +1,57 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 from .services import get_arena_group_name
+from .domain.arena import Arena
+from .domain.player import Player
+import json
+import asyncio
+from config.services import get_user_name
+from .domain.arena_manager import ArenaManager
 
 class ArenaConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.arena_id = self.scope['url_route']['kwargs']['arena_id']
         self.arena_group_name = get_arena_group_name(self.arena_id)
+        self.user_id = self.scope.get('user_id')
+        self.token = self.scope.get('token')
+        self.user_name = await get_user_name(self.user_id, self.token)
+        
+        # redis에서 참가자 명단에 있는지 검증해야 함
         
         await self.accept()
+        await self.channel_layer.group_add(self.arena_group_name, self.channel_name)
         
-        await self.channel_layer.group_add(
-            self.reception_group_name,
-            self.channel_name
-        )
+        self.arena:Arena = ArenaManager.get_arena(self.arena_id)
+        player = Player(self.user_id, self.arena)
+        team = await self.arena.add_player(player, self.broadcast_message)
+        await self.send_json({
+            'type': 'team',
+            'message': team.value
+            })
         
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
-            self.reception_group_name,
+            self.arena_group_name,
             self.channel_name
         )
         
     async def receive(self, text_data):
         return
         
-    async def broadcast_message(self, message_type, content):
+    async def broadcast_message(self, message_type, message):
         await self.channel_layer.group_send(
-            self.reception_group_name,
+            self.arena_group_name,
             {
                 'type': 'send_to_client',
                 'message_type': message_type,
-                'content': content
+                'message': message
             }
         )
 
     async def send_to_client(self, event):
-        await self.send(text_data=json.dumps({
+        await self.send_json({
             'type': event['message_type'],
-            'content': event['content']
-        }))
+            'message': event['message']
+        })
+        
+    async def send_json(self, message):
+        await self.send(text_data=json.dumps(message))
