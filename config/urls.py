8666a33685c055e8d1abe9067a306549469dcd6b
AhnJoonSung <tjdtna01@naver.com>
2024-12-22 02:08:15 +0900

    :sparkles: feat: Tournament Create API

--- config/urls.py
+++ config/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/reception/', include('reception.urls')),
+    path('api/tournament/', include('tournament.urls')),
 ]
--- tournament/views.py
+++ tournament/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import TournamentSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class TournamentCreateView(APIView):
+    def post(self, request):
+        serializer = TournamentSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "message": "Tournament created.",
+                "data": serializer.data
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+# class TournamentDetailView(APIView):
+    
+        
+# class TournamentListView(APIView):
+    
+
+# class TournamentJoinView(APIView):
+    
+
+# class TournamentStartView(APIView):
