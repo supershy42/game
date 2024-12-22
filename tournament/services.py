a8d6c48e7720c00709e6629fdf01808a987764f9
AhnJoonSung <tjdtna01@naver.com>
2024-12-22 16:22:25 +0900

    :sparkles: feat: 서비스 함수들 동기로 변경
    비동기 context 내에서 필드 설정 같은 ORM 구문이 지원이 안돼서 동기로
    변경

--- tournament/services.py
+++ tournament/services.py
@@ -5,6 +5,7 @@ import asyncio
 from config.services import UserService
 from config.custom_validation_error import CustomValidationError
 from config.error_type import ErrorType
+from asgiref.sync import async_to_sync
 
 class TournamentService:
     @staticmethod
@@ -12,57 +13,47 @@ class TournamentService:
         return f"tournament_{tournament_id}"
     
     @staticmethod
-    async def get_current_participants(tournament_id: int):
-        return await TournamentParticipant.objects.filter(tournament_id=tournament_id).acount()
-    
-    @staticmethod
-    async def is_full(tournament: Tournament):
-        current_count = await TournamentParticipant.objects.filter(tournament=tournament).acount()
-        return current_count == tournament.max_participants
-
-    @staticmethod
-    async def join(tournament_id, user_id):
-        tournament = await Tournament.objects.aget(id=tournament_id)
+    def join(tournament_id, user_id):
+        tournament = Tournament.objects.get(id=tournament_id)
         
-        if await TournamentParticipant.objects.filter(tournament=tournament, user_id=user_id).aexists():
-            raise CustomValidationError(ErrorType.ALREADY_EXISTS)
-        if await TournamentService.is_full(tournament):
+        if tournament.state != tournament.State.WAITING:
+            raise CustomValidationError(ErrorType.TOURNAMENT_NOT_WAITING)
+        if tournament.is_full():
             raise CustomValidationError(ErrorType.TOURNAMENT_FULL)
-        if tournament.state != tournament.State.WAITING:
-            raise CustomValidationError(ErrorType.TOURNAMENT_NOT_WAITING)
+        if TournamentParticipant.objects.filter(tournament=tournament, user_id=user_id).exists():
+            raise CustomValidationError(ErrorType.ALREADY_EXISTS)
         
-        await TournamentParticipant.objects.acreate(tournament=tournament, user_id=user_id)
+        TournamentParticipant.objects.create(tournament=tournament, user_id=user_id)
 
     @staticmethod
-    async def start(tournament_id):
-        tournament = await Tournament.objects.aget(id=tournament_id)
+    def start(tournament_id, user_id):
+        tournament = Tournament.objects.get(id=tournament_id)
         
+        if tournament.creator != user_id:
+            raise CustomValidationError(ErrorType.PERPISSON_DENIED)
         if tournament.state != tournament.State.WAITING:
             raise CustomValidationError(ErrorType.TOURNAMENT_NOT_WAITING)
-        if not await TournamentService.is_full(tournament):
+        if not tournament.is_full():
             raise CustomValidationError(ErrorType.TOURNAMENT_NOT_FULL)
         
-        await TournamentService.set_players(tournament)
+        TournamentService.set_players(tournament)
+        tournament.state = Tournament.State.IN_PROGRESS
+        tournament.save()
         
-        user_ids = await TournamentService.get_participant_ids(tournament)
-        tasks = [
-            UserService.send_notification(user_id, {
+        user_ids = TournamentService.get_participant_ids(tournament)
+        for user_id in user_ids:
+            async_to_sync(UserService.send_notification)(user_id, {
                 "type":"tournament.start",
                 "url": TournamentService.get_grooup_name(tournament_id),
                 "message": f"The tournament \"{tournament.name}\" is starting!"
             })
-            for user_id in user_ids
-        ]
-        await asyncio.gather(*tasks)
         
     @staticmethod
-    async def update_bracket(tournament_id):
-        tournament = await Tournament.objects.aget(id=tournament_id)
-        all_matches = await Match.objects.filter(tournament=tournament).all()
+    def update_bracket(tournament_id):
+        tournament = Tournament.objects.get(id=tournament_id)
+        all_matches = Match.objects.filter(tournament=tournament).all()
         pending_matches = [match for match in all_matches if match.state == Match.State.PENDING]
         match_dict = {match.match_number: match for match in all_matches}
-        
-        tasks = []
         
         for match in pending_matches:
             match_number = match.match_number
@@ -81,40 +72,31 @@ class TournamentService:
                 
             if match.left_player and match.right_player:
                 match.state = Match.State.READY
-                tasks.append(match.asave())
-            
-        await asyncio.gather(*tasks)
+                match.save()
 
     @staticmethod
-    async def set_players(tournament:Tournament):
-        user_ids = await TournamentService.get_participant_ids(tournament)
-        matches:List[Match] = await TournamentService.create_matches(tournament)
+    def set_players(tournament:Tournament):
+        user_ids = TournamentService.get_participant_ids(tournament)
+        matches:List[Match] = TournamentService.create_matches(tournament)
         max_participants = tournament.max_participants
-        tasks = []
         
-        async def process_match(match_number: int):
+        for match_number in range(max_participants // 2, max_participants):
             match = matches[match_number - 1]
             i = match_number % max_participants // 2
             match.left_player = user_ids[i]
             match.right_player = user_ids[i + 1]
             match.state = match.State.READY
-            await match.asave()
-            
-        for match_number in range(max_participants // 2, max_participants):
-            tasks.append(process_match(match_number))
-            
-        await asyncio.gather(*tasks)
 
     @staticmethod    
-    async def create_matches(tournament: Tournament):
+    def create_matches(tournament: Tournament):
         matches = []
         for match_number in range(1, tournament.max_participants):
             round = tournament.total_rounds - int(math.log2(match_number))
-            match = await Match.objects.acreate(tournament=tournament, round=round, match_number=match_number)
+            match = Match.objects.create(tournament=tournament, round=round, match_number=match_number)
             matches.append(match)
             
         return matches
     
     @staticmethod
-    async def get_participant_ids(tournament: Tournament):
-        return await TournamentParticipant.objects.filter(tournament=tournament).values_list('user_id', flat=True)
+    def get_participant_ids(tournament: Tournament):
+        return TournamentParticipant.objects.filter(tournament=tournament).values_list('user_id', flat=True)
