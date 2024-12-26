c4a8433cb0c72e2f532e1235990036e15509bc77
AhnJoonSung <tjdtna01@naver.com>
2024-12-26 18:29:46 +0900

    :lipstick: style: 주석 제거

--- config/settings.py
+++ config/settings.py
@@ -78,12 +78,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 if DATABASE_ENGINE == 'sqlite3':
     DATABASES = {
         'default': {
