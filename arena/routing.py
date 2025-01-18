a9ac615a1716d38b1d6f62dc09255b7bb28a0cde
AhnJoonSung <tjdtna01@naver.com>
2025-01-18 13:37:04 +0900

    :sparkles: feat: websocket 경로도 수정

--- arena/routing.py
+++ arena/routing.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .consumers import ArenaConsumer
 
 websocket_urlpatterns = [
-    path("ws/arena/<int:arena_id>/", ArenaConsumer.as_asgi(), name="websocket_arena"),
-    path("ws/tournament/<int:tournament_id>/match/<int:match_number>/", ArenaConsumer.as_asgi(), name="websocket_tournament_match"),
+    path("ws/game/arena/<int:arena_id>/", ArenaConsumer.as_asgi(), name="websocket_arena"),
+    path("ws/game/tournament/<int:tournament_id>/match/<int:match_number>/", ArenaConsumer.as_asgi(), name="websocket_tournament_match"),
 ]
--- reception/routing.py
+++ reception/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .consumers import ReceptionConsumer
 
 websocket_urlpatterns = [
-    path("ws/reception/<int:reception_id>/", ReceptionConsumer.as_asgi(), name="websocket_reception"),
+    path("ws/game/reception/<int:reception_id>/", ReceptionConsumer.as_asgi(), name="websocket_reception"),
 ]
