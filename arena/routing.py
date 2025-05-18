60e79a5a6852510bbd222f1da25529591ca14696
sujin Park <chemd78@naver.com>
2025-05-18 23:16:30 +0900

    :bug: fix: arena unique id를 str 자료형으로 수정

--- arena/routing.py
+++ arena/routing.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .consumers import ArenaConsumer
 
 websocket_urlpatterns = [
-    path("ws/game/arena/<int:arena_id>/", ArenaConsumer.as_asgi(), name="websocket_arena"),
+    path("ws/game/arena/<str:arena_id>/", ArenaConsumer.as_asgi(), name="websocket_arena"),
     path("ws/game/tournament/<int:tournament_id>/match/<int:match_number>/", ArenaConsumer.as_asgi(), name="websocket_tournament_match"),
 ]
